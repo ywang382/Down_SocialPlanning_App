f365934ea6908ba04ddef26cb1db955380223548
vance5427 <327Valley!>
2019-04-10 18:01:27 -0400

    Linear Layout for add friend

--- app/src/main/java/com/example/down/AddFriendActivity.java
+++ app/src/main/java/com/example/down/AddFriendActivity.java
@@ -15,15 +15,8 @@ public class AddFriendActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_friend);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        setTitle("Add Friends");
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
