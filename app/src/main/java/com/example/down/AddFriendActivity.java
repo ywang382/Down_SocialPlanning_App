f39c8a5251deb707e2e99408f53d51c2c04ab14c
vance5427 <327Valley!>
2019-04-11 19:38:56 -0400

    ListView initialized

--- app/src/main/java/com/example/down/AddFriendActivity.java
+++ app/src/main/java/com/example/down/AddFriendActivity.java
@@ -6,8 +6,16 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class AddFriendActivity extends AppCompatActivity {
+
+    ListView friendSearch;
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +25,18 @@ public class AddFriendActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         setTitle("Add Friends");
 
+        friendSearch = findViewById(R.id.friendsearch);
+
+        ArrayList<String> arrayFriends = new ArrayList<>();
+        arrayFriends.addAll(Arrays.asList(getResources().getStringArray(R.array.myFriends)));
+
+        adapter = new ArrayAdapter<String>(
+                AddFriendActivity.this,
+                android.R.layout.simple_list_item_1,
+                arrayFriends
+        );
+
+        friendSearch.setAdapter(adapter);
     }
 
 }
