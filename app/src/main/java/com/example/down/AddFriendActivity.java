4097788564b640ed12ea9638637fef4c7f02b996
vance5427 <327Valley!>
2019-04-14 09:47:54 -0400

    Back button add friends

--- app/src/main/java/com/example/down/AddFriendActivity.java
+++ app/src/main/java/com/example/down/AddFriendActivity.java
@@ -3,6 +3,7 @@ package com.example.down;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -16,6 +17,9 @@ public class AddFriendActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         setTitle("Add Friends");
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
     }
 
 }
