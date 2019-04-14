e71f11ffe73a728651045b27b8598be08d2c9a11
cldelahan <cldelahan@gmail.com>
2019-04-14 18:42:46 -0400

    Created PersonalUserProfile activity.

---
+++ app/src/main/java/com/example/down/PersonalUserProfile.java
@@ -0,0 +1,13 @@
+package com.example.down;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PersonalUserProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_user_profile);
+    }
+}
