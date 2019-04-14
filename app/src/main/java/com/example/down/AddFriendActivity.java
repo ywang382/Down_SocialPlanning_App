51d7930bbb3d2e4774018e6b328756e9d1afa674
vance5427 <327Valley!>
2019-04-14 10:23:20 -0400

    Search bar works. not over database yet

--- app/src/main/java/com/example/down/AddFriendActivity.java
+++ app/src/main/java/com/example/down/AddFriendActivity.java
@@ -5,10 +5,21 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class AddFriendActivity extends AppCompatActivity {
+
+    SearchView searchBar;
+    ListView searchFriends;
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +31,34 @@ public class AddFriendActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        searchFriends = findViewById(R.id.friendsearch);
+        ArrayList<String> friendsArray = new ArrayList<>();
+        friendsArray.addAll(Arrays.asList(getResources().getStringArray(R.array.friendsArray)));
+
+        adapter = new ArrayAdapter<String>(
+                AddFriendActivity.this,
+                android.R.layout.simple_list_item_1,
+                friendsArray
+        );
+
+        searchFriends.setAdapter(adapter);
+
+        searchBar = findViewById(R.id.searchBar);
+        searchBar.setIconified(false);
+        searchBar.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                adapter.getFilter().filter(s);
+                return false;
+            }
+        });
+
     }
 
 }
