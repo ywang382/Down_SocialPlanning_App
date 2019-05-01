feb05b0710674268777cdd79852ea7cff8af15c7
trenton-uima <ywang382@jhu.edu>
2019-05-01 11:51:07 -0400

    First implementation of app tutorial for first-time users.

---
+++ app/src/main/java/com/example/down/TutorialActivity.java
@@ -0,0 +1,48 @@
+package com.example.down;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class TutorialActivity extends AppCompatActivity {
+    private Button tutorial;
+    private Button skip;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        final SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        setContentView(R.layout.tutorial);
+        getSupportActionBar().hide();
+        tutorial = (Button) findViewById(R.id.button_tutorial);
+        skip = (Button) findViewById(R.id.button_skip);
+
+        tutorial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        skip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(TutorialActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+                sharedPreferences.edit().putBoolean("Completed_Tutorial", true).commit();
+            }
+        });
+
+    }
+}
--- app/src/main/java/com/example/down/WelcomeActivity.java
+++ app/src/main/java/com/example/down/WelcomeActivity.java
@@ -1,6 +1,8 @@
 package com.example.down;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
@@ -15,16 +17,27 @@ public class WelcomeActivity extends AppCompatActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState){
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
         getSupportActionBar().hide();
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(WelcomeActivity.this, LoginActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        }, SPLASH_TIME_OUT);
+        if (!sharedPreferences.getBoolean("Completed_Tutorial", false)) {
+            // The user hasn't seen the OnboardingFragment yet, so show it
+            Intent intent = new Intent(WelcomeActivity.this, TutorialActivity.class);
+            startActivity(intent);
+            finish();
+        } else {
+
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent intent = new Intent(WelcomeActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }, SPLASH_TIME_OUT);
+
+        }
     }
 }
